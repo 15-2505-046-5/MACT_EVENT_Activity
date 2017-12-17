8831fbdb6f8d51ed1ca84ac3df93fb85e5aad887
15-2505-070-5 <v070ff@yamaguchiu.ac.jp>
2017-12-17 11:31:13 +0900

    閲覧画面からfabと画像を削除

--- app/src/main/java/com/example/enpit_p15/mact_event/ShowEventActivity.java
+++ app/src/main/java/com/example/enpit_p15/mact_event/ShowEventActivity.java
@@ -1,20 +1,15 @@
 /*閲覧機能の実装。ほかに記事の共有？と画像から主要職を取得して画像デザインに反映も実装？要らないと思われる。p329-p333
   Realmデータベースからの検索など重要そうなことを結構やっているので要確認*/
+/*fabを削除。画像はlayoutからは消したけどimage関連のコードは使うかもしれないからコメントアウト*/
 package com.example.enpit_p15.mact_event;
 
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import io.realm.Realm;
@@ -34,18 +29,6 @@ public class ShowEventActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /*記事の共有*/
-                Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                shareIntent.putExtra(Intent.EXTRA_TEXT,mBodyText);
-                shareIntent.setType("text/plain");
-                startActivity(shareIntent);
-                /*ここまで*/
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mRealm = Realm.getDefaultInstance();
@@ -54,7 +37,7 @@ public class ShowEventActivity extends AppCompatActivity {
         final long eventId = intent.getLongExtra(EVENT_ID, ERR_CD);
 
         TextView body = (TextView)findViewById(R.id.body);
-        ImageView imageView = (ImageView) findViewById(R.id.toolbar_image);
+        //ImageView imageView = (ImageView) findViewById(R.id.toolbar_image);
         NestedScrollView scrollView =
                 (NestedScrollView)findViewById(R.id.scroll_view);
 
@@ -72,23 +55,9 @@ public class ShowEventActivity extends AppCompatActivity {
         byte[] bytes =  event.image;
         if(bytes != null && bytes.length > 0){
             mBitmap = MyUtils.getImageFromByte(bytes);
-            imageView.setImageBitmap(mBitmap);
+            //imageView.setImageBitmap(mBitmap);
         /*ここまで*/
 
-            /*日記の画像から代表色を取得して画面に反映*/
-            Palette palette = Palette.from(mBitmap).generate();
-
-            int titleColor = palette.getLightVibrantColor(Color.WHITE);
-            int bodyColor = palette.getDarkMutedColor(Color.BLACK);
-            int scrimColor = palette.getMutedColor(Color.DKGRAY);
-            int iconColor = palette.getLightMutedColor(Color.LTGRAY);
-
-            layout.setExpandedTitleColor(titleColor);
-            layout.setContentScrimColor(scrimColor);
-            scrollView.setBackgroundColor(bodyColor);
-            body.setTextColor(titleColor);
-            fab.setBackgroundTintList(ColorStateList.valueOf(iconColor));
-            /*ここまで*/
         }
     }
 
